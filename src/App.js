0e8b7aad53b95c5eae53f167f2dcdac845b086ea
LordBerbeross <mustafaberber020@gmail.com>
2019-03-18 20:57:54 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import colors from './colors.js';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      QuoteList: '',
+      AuthorList: '',
+      quotes: [],
+      authors: [],
+
+    }
+  }
+  componentDidMount(){
+    let rand= (Math.round(Math.random()*102));
+     fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          
+          quotes: data.quotes.map(val=>val.quote),
+          authors: data.quotes.map(val=>val.author),
+          QuoteList: data.quotes[rand].quote,
+          AuthorList: data.quotes[rand].author
+        })
+       
+      })
+      
+ 
+  }
+  toggleButton=()=>{
+
+      let rand=(Math.round(Math.random()*102));
+      let randc=(Math.round(Math.random()*3))
+      document.querySelector("body").style.background = colors[randc];
+      document.querySelector("body").style.transition = "background 2s";
+
+
+      this.setState({ 
+        QuoteList:this.state.quotes[rand],
+        AuthorList:this.state.authors[rand]
+      })
+     
+  }
+  render(){
+ 
+    return(
+
+       <div className="wrapper">
+        <div id="quote-box">
+        <div id="text">{this.state.QuoteList}
+        </div>
+        <div id="author">-- 
+   {this.state.AuthorList} 
+          </div>
+       <div className="buttons">
+ <a id="tweet-quote" href={`https://twitter.com/intent/tweet?text=${this.state.QuoteList}--${this.state.AuthorList}`} target="_blank" rel="noopener noreferrer" ><i class="fab fa-twitter"></i></a>
+ <button id="new-quote" onClick={this.toggleButton}>New quote</button>
       </div>
-    );
+   
+        </div>
+        
+        
+      
+       </div>
+    )
   }
 }
+
 
 export default App;
---
+++ src/colors.js
@@ -0,0 +1,25 @@
+const colors =[
+"linear-gradient(to right,#36626f,#c1a24e)",
+"linear-gradient(to right,#a43d23,#55f890)",
+"linear-gradient(to right,#82561b,#ec47b0)",
+"linear-gradient(to right,#a9bbf2,#4d6a25)",
+"linear-gradient(to right,#05c6ef,#41df8d)",
+"linear-gradient(to right,#057d3b,#d8c3df)",
+"linear-gradient(to right,#825347,#eab005)",
+"linear-gradient(to right,#8cac44,#1ef9c6)",
+"linear-gradient(to right,#c7d852,#d51871)",
+"linear-gradient(to right,#db4661,#d8758c)",
+"linear-gradient(to right,#fe8147,#3b9c39)",
+"linear-gradient(to right,#d7e5de,#899fa3)",
+"linear-gradient(to right,#e4b7c6,#e0747b)",
+"linear-gradient(to right,#9a0fc5,#87749d)",
+"linear-gradient(to right,#dc1e4d,#85b5ed)",
+"linear-gradient(to right,#d3f9f7,#4eeeea)",
+"linear-gradient(to right,#b5a24f,#50c5ab)",
+"linear-gradient(to right,#a705f3,#5f1613)",
+"linear-gradient(to right,#dc2f4f,#e4a817)",
+"linear-gradient(to right,#1c7128,#c6785f)"
+
+]
+
+export default colors;
